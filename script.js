1c949d92b4b1824dac9aec462726a657c4c818c0
maniO12 <159547667+maniO12@users.noreply.github.com>
2024-02-10 12:37:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+ function opentab(tabname){
+      for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+      }
+      document.getElementById(tabname).classList.add("active-tab");
+ }
